7612a9562fc0d6cf3a85a0fa5233db3ac932a80d
The-Estonian <Zaar2213@gmail.com>
2023-09-13 23:19:25 +0300

    go-reloaded 50% done

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module 01.kood.tech/git/jsaar/go-reloaded
 
 go 1.20
+
+require golang.org/x/text v0.13.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello!")
+	data, error := os.ReadFile("sample.txt")
+	splitData := strings.Split(string(data), " ")
+	if error != nil {
+		panic("Error with file")
+	} else {
+		var newStringList []string
+		counter := 0
+		for i := 0; i < len(splitData)-1; i++ {
+			if splitData[i+1] == "(up)" {
+				newStringList = append(newStringList, strings.ToUpper(splitData[i]))
+				i++
+			} else if strings.HasPrefix(splitData[i+1], "(low,") {
+				counter, _ = strconv.Atoi(string(splitData[i+2][0]))
+				fmt.Println(counter)
+			} else if splitData[i+1] == "(low)" {
+				newStringList = append(newStringList, strings.ToLower(splitData[i]))
+				i++
+			} else if splitData[i+1] == "(bin)" {
+				number, _ := strconv.ParseInt(splitData[i], 2, 64)
+				newStringList = append(newStringList, strconv.FormatInt(number, 10))
+				i++
+			} else if splitData[i+1] == "(hex)" {
+				number, _ := strconv.ParseInt(splitData[i], 16, 64)
+				newStringList = append(newStringList, strconv.FormatInt(number, 10))
+				i++
+			} else if splitData[i+1] == "(cap)" {
+				newStringList = append(newStringList, cases.Title(language.Und, cases.NoLower).String(splitData[i]))
+				i++
+			} else {
+				newStringList = append(newStringList, splitData[i])
+			}
+		}
+		newStringList = append(newStringList, splitData[len(splitData)-1])
+		fmt.Println(newStringList)
+		newFile, error := os.Create("result.txt")
+		if error != nil {
+			panic("Error creating file")
+		} else {
+			newFile.WriteString("newStringList")
+		}
+	}
 }
